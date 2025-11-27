0a1eab061b0a0efe95a97161e84f6b3ef52a6941
juan <anstmdwn34@gmail.com>
2025-11-27 22:27:55 +0900

    refactor(utils): 증강관련 네이밍 오류 개선

--- src/utils/exception.py
+++ src/utils/exception.py
@@ -4,7 +4,7 @@ class DataProcessingError(Enum):
     """데이터 처리 에러 코드 및 메시지"""
     DATA_DEDUPLICATION_ERROR = "DATA_DEDUPLICATION_ERROR"
     DATA_FILTERING_ERROR = "DATA_FILTERING_ERROR"
-    DATA_ARGUMENTATION_ERROR = "DATA_ARGUMENTATION_ERROR"
+    DATA_AUGMENTATION_ERROR = "DATA_AUGMENTATION_ERROR"
 
 class BaseDataProcessingException(Exception):
     """데이터 처리 공통 예외"""
@@ -26,7 +26,7 @@ class DataFilteringError(BaseDataProcessingException):
         super().__init__(error, status_code=status_code)
 
 
-class DataArgumentationError(BaseDataProcessingException):
+class DataAugmentationError(BaseDataProcessingException):
     """데이터 증강 예외"""
-    def __init__(self, error: DataProcessingError.DATA_ARGUMENTATION_ERROR, status_code: int = 500):
+    def __init__(self, error: DataProcessingError.DATA_AUGMENTATION_ERROR, status_code: int = 500):
         super().__init__(error, status_code=status_code)
